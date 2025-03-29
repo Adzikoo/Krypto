5cf477f261b2cc41e0d465ef10bdb67943d0e842
252960 Adrian Urbanczyk <252960@edu.p.lodz.pl>
2025-03-29 20:02:40 +0100

    Zabije sie przysiegam kurwa

--- src/main/java/org/example/krypto/KryptoApplication.java
+++ src/main/java/org/example/krypto/KryptoApplication.java
@@ -23,39 +23,83 @@ public class KryptoApplication extends Application {
 
     public static void main(String[] args) {
         //launch();
-        DES.Key key1 = new DES.Key("01234567".getBytes());
-        DES.Key key2 = new DES.Key("87654321".getBytes());
-        DES.Key key3 = new DES.Key("43215678".getBytes());
-
-        String s = "Adrianur";
-        //szyfruj tekst
-        byte[] szyfr1 = DES.encrypt(s.getBytes(), key1);
-        byte[] odszyfr1 = DES.decrypt(szyfr1, key2);
-        byte[] szyfr2 = DES.encrypt(odszyfr1, key3);
-        //odszyfruj tekst
-        byte[] odszyfr2 = DES.decrypt(szyfr2, key3);
-        byte[] szyfr3 = DES.encrypt(odszyfr2, key2);
-        byte[] odszyfr3 = DES.decrypt(szyfr3, key1);
+//        DES.Key key1 = new DES.Key("01234567".getBytes());
+//        DES.Key key2 = new DES.Key("87654321".getBytes());
+//        DES.Key key3 = new DES.Key("43215678".getBytes());
+//
+//        String s = "A";
 
 
 
+//        //szyfruj tekst
+//        byte[] szyfr1 = DES.encrypt(s.getBytes(), key1);
+//        byte[] odszyfr1 = DES.decrypt(szyfr1, key2);
+//        byte[] szyfr2 = DES.encrypt(odszyfr1, key3);
+//        //odszyfruj tekst
+//        byte[] odszyfr2 = DES.decrypt(szyfr2, key3);
+//        byte[] szyfr3 = DES.encrypt(odszyfr2, key2);
+//        byte[] odszyfr3 = DES.decrypt(szyfr3, key1);
 
-        // wypisz tekst jawny
-        System.out.println("Tekst jawny: " + s);
-        //wypisz klucz w postaci hex
-        //System.out.println("Klucz 1: " + key.getBits());
-        // wypisz tekst zaszyfrowany
-        System.out.println("Tekst zaszyfrowany: " + new String(szyfr1));
-        // wypisz tekst odszyfrowany
-        System.out.println("Tekst odszyfrowany: " + new String(odszyfr1));
-        // wypisz tekst zasztfrowany
-        System.out.println("Tekst zaszyfrowany: " + new String(szyfr2));
-        // wypisz tekst odszyfrowany
-        System.out.println("Tekst odszyfrowany: " + new String(odszyfr2));
-        // wypisz tekst zaszyfrowany
-        System.out.println("Tekst zaszyfrowany: " + new String(szyfr3));
-        // wypisz tekst odszyfrowany
-        System.out.println("Tekst odszyfrowany: " + new String(odszyfr3));
+//
+
+
+
+
+
+//        // wypisz tekst jawny
+//        System.out.println("Tekst jawny: " + s);
+//        //wypisz klucz w postaci hex
+//        //System.out.println("Klucz 1: " + key.getBits());
+//        // wypisz tekst zaszyfrowany
+//        System.out.println("Tekst zaszyfrowany: " + new String(szyfr1));
+//        // wypisz tekst odszyfrowany
+//        System.out.println("Tekst odszyfrowany: " + new String(odszyfr1));
+//        // wypisz tekst zasztfrowany
+//        System.out.println("Tekst zaszyfrowany: " + new String(szyfr2));
+//        // wypisz tekst odszyfrowany
+//        System.out.println("Tekst odszyfrowany: " + new String(odszyfr2));
+//        // wypisz tekst zaszyfrowany
+//        System.out.println("Tekst zaszyfrowany: " + new String(szyfr3));
+//        // wypisz tekst odszyfrowany
+//        System.out.println("Tekst odszyfrowany: " + new String(odszyfr3));
+
+
+//        //szyfruj tekst
+//        byte[] szyfr1 = DES.encrypt(s.getBytes(), key1);
+//        //deszyfruj tekst
+//
+//        byte[] odszyfr1 = DES.decrypt(szyfr1, key1);
+//        //wypisz tekst jawny
+//        System.out.println("Tekst jawny: " + s);
+//        //wypisz klucz w postaci hex
+//        //System.out.println("Klucz 1: " + key.getBits());
+//        // wypisz tekst zaszyfrowany
+//        System.out.println("Tekst zaszyfrowany: " + new String(szyfr1));
+//        // wypisz tekst odszyfrowany
+//        System.out.println("Tekst odszyfrowany: " + new String(odszyfr1));
+
+// Define three keys
+        byte[] key1 = "01234567".getBytes();
+        byte[] key2 = "87654321".getBytes();
+        byte[] key3 = "43215678".getBytes();
+
+        // Create a TripleDES.Key object
+        TripleDES.Key tripleKey = new TripleDES.Key(key1, key2, key3);
+
+        // Sample text to encrypt and decrypt
+        String text = "Hel";
+        byte[] textBytes = text.getBytes();
+
+        // Encrypt the text
+        byte[] encryptedData = TripleDES.encrypt3D(textBytes, tripleKey);
+
+        // Decrypt the text
+        byte[] decryptedData = TripleDES.decrypt3D(encryptedData, tripleKey);
+
+        // Print the results
+        System.out.println("Original text: " + text);
+        System.out.println("Encrypted text: " + Arrays.toString(encryptedData));
+        System.out.println("Decrypted text: " + new String(decryptedData));
 
 
 
--- src/main/java/org/example/krypto/TripleDES.java
+++ src/main/java/org/example/krypto/TripleDES.java
@@ -1,17 +1,41 @@
 package org.example.krypto;
 
-public class TripleDES {
+import java.util.Arrays;
 
-    public static byte[] encrypt3DES(byte[] message, DES.Key key1, DES.Key key2, DES.Key key3) {
-        byte[] result = DES.encrypt(message, key1);
-        result = DES.decrypt(result, key2);
-        result = DES.encrypt(result, key3);
-        return result;
+public class TripleDES {
+    public static class Key {
+        private DES.Key key1;
+        private DES.Key key2;
+        private DES.Key key3;
+
+        public Key(byte[] key1, byte[] key2, byte[] key3) {
+            this.key1 = new DES.Key(key1);
+            this.key2 = new DES.Key(key2);
+            this.key3 = new DES.Key(key3);
+        }
+
+        public DES.Key getKey1() {
+            return key1;
+        }
+
+        public DES.Key getKey2() {
+            return key2;
+        }
+
+        public DES.Key getKey3() {
+            return key3;
+        }
     }
-    public static byte[] decrypt3DES(byte[] cipher, DES.Key key1, DES.Key key2, DES.Key key3) {
-        byte[] result = DES.decrypt(cipher, key3);
-        result = DES.encrypt(result, key2);
-        result = DES.decrypt(result, key1);
-        return result;
+
+    public static byte[] encrypt3D(byte[] data, Key key) {
+        byte[] encryptedData = DES.encrypt(data, key.getKey1());
+        byte[] decryptedData = DES.decrypt(encryptedData, key.getKey2());
+        return DES.encrypt(decryptedData, key.getKey3());
+    }
+
+    public static byte[] decrypt3D(byte[] data, Key key) {
+        byte[] decryptedData = DES.decrypt(data, key.getKey3());
+        byte[] encryptedData = DES.encrypt(decryptedData, key.getKey2());
+        return DES.decrypt(encryptedData, key.getKey1());
     }
 }
